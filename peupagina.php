9a7d81d20acb6ddcce0a6d0c65c95b31a617de97
root <root@daw2m08.fjeclot.net>
2018-04-25 19:55:48 +0200

    Commit 1!

---
+++ peupagina.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Peu de pagina per defecte per a noves entrades
+Plugin URI: https://github.com/lablll1718daw2/pluginwp2_laibla.git
+Description: Crea peus de pàgina per defecte per a noves entrades
+Version: 0.1
+Author: Laia Blanco Llaveria
+Author URI: https://github.com/lablll1718daw2
+License: GNU GPLv3
+*/
+?>
